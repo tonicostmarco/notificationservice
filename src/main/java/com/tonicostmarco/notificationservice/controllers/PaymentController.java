49ba123f87e166f33f9b39ad172b54e867858591
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-07 20:11:14 -0300

    feat(controller): POST /payments endpoint routing to exchange by payment status

--- src/main/java/com/tonicostmarco/notificationservice/controllers/PaymentController.java
+++ src/main/java/com/tonicostmarco/notificationservice/controllers/PaymentController.java
@@ -1,4 +1,31 @@
 package com.tonicostmarco.notificationservice.controllers;
 
+import com.tonicostmarco.notificationservice.model.dtos.PaymentDTO;
+import jakarta.validation.Valid;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/payments")
 public class PaymentController {
+
+
+    private final RabbitTemplate template;
+
+    public PaymentController(RabbitTemplate template) {
+        this.template = template;
+    }
+
+    @PostMapping
+    public ResponseEntity<PaymentDTO> receivePayment(@RequestBody @Valid PaymentDTO dto) {
+        template.convertAndSend("exchange", "payment." + dto.status(), dto);
+
+        return ResponseEntity.ok(dto);
+
+    }
+
 }
